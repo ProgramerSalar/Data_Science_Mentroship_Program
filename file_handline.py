886da20401814cbc13200f82faf5ad3eb7e645e7
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-10 16:32:04 +0530

    write the text in file

--- file_handline.py
+++ file_handline.py
@@ -1,1 +1,11 @@
-print('hello world')
+# case 1 if the file is not present 
+#1. open the file
+#2. Read/write the data 
+#3. close the file 
+ 
+f = open('sample.txt','w')      # sample.txt is file name, w means write # open the file 
+f.write('hello world')          # write the file 
+f.close()
+
+# if you close the file then write give the error I/O operation on closed file.
+# f.write('hello hero')
