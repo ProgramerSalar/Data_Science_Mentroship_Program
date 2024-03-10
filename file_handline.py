192035880085e98d9526889f9824aa397e307fea
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-10 16:38:17 +0530

    write a multiple lines in one file

--- file_handline.py
+++ file_handline.py
@@ -3,9 +3,17 @@
 #2. Read/write the data 
 #3. close the file 
  
-f = open('sample.txt','w')      # sample.txt is file name, w means write # open the file 
-f.write('hello world')          # write the file 
-f.close()
+# f = open('sample.txt','w')      # sample.txt is file name, w means write # open the file 
+# f.write('hello world')          # write the file 
+# f.close()
 
 # if you close the file then write give the error I/O operation on closed file.
 # f.write('hello hero')
+
+
+# write a multiple lines in one file 
+f = open('sample.txt','w')
+f.write('hello hero')
+f.write('\nhow are you')
+f.close()
+
