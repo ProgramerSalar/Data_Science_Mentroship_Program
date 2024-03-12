7c32843741737fc60ee0ea7970dac8478d70e05b
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-12 19:01:47 +0530

    what is iteration

---
+++ Iterator/one.py
@@ -0,0 +1,6 @@
+#What is an Iteration
+# Iteration is a general term for taking each item of something, one after another. Any time you use a loop, explicit or implicit, to go over a group of items, that is iteration.
+
+n = [1, 2, 3]
+for i in n:
+    print(i)
