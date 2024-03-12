8bbfc333be3088c4f2ad9f563971822c2f96dbdb
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-12 15:44:16 +0530

    decorator

--- Decoretors/one.py
+++ Decoretors/one.py
@@ -1,11 +1,28 @@
+# A decorator in python is a function that receives another function as input and adds some functionality(decoration) to and it and returns it.
+# This can happen only because python functions are 1st class citizens.
+
+# def modify(func, num):
+#     return func(num)
+
+# def square(num):
+#     return num ** 2
+
+# a = modify(square, 2)
+# print(a)
 
 
-def modify(func, num):
-    return func(num)
 
-def square(num):
-    return num ** 2
+def my_decoretor(func):
+    def my_function():
+        print("*********")
+        func()
+        print("******")
+
+    return my_function()
 
 
-a = modify(square, 2)
-print(a)
+def hello():
+    print('hello')
+
+
+my_decoretor(hello)
