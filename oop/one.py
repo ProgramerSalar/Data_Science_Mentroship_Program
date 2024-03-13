c701a64133d3a5ff45fe9686cc2ef2e315c23e5b
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-13 17:59:36 +0530

    deffrences of method and class

--- oop/one.py
+++ oop/one.py
@@ -1,93 +1,47 @@
+#
 
-
-
-class Atm:
-
-    # constructor 
-    # custructor are atomatically call when excute the class 
-    def __init__(self):
-        # print("hello world")
-        self.pin = ''
-        self.balance = 0
-        self.func()
-
-
-    # function 
-    def func(self):
-        user_input = input("""
-            Hi how can help you
-            1. Press 1 To create Pin
-            2. Press 2 TO change Pin
-            3. Press 3 To check Balance
-            4. Press 4 To withdraw
-            5. Anything To Exit.
-        """)
-        
-        if user_input == '1':
-            self.create_pin()
-            
-        if user_input == '2':
-            self.change_pin()
-            
-            
-        if user_input == '3':
-            self.check_balance()
-            
-        if user_input == '4':
-            self.withdrawl()
-            
-        
-            
-            
-        
-    def create_pin(self):
-        user_pin = input("ENter Pin")
-        balance = int(input("Enter Balance"))
-        self.pin = user_pin
-        self.balance = balance
-        print("Pin created Successfully")
-        self.func()
-        
-        
-    def change_pin(self):
-        old_pin = input("Enter Old Pin")
-        if old_pin == self.pin:
-            new_pin = input("Enter New Pin")
-            self.pin = new_pin
-            print("pin Updated Successfully")
-            self.func()
-        else:
-            print("invalid pin")
-            self.func()
-            
-            
-    def check_balance(self):
-        user_pin = input("Enter Pin")
-        # if user_pin == self.pin:
-        print(self.balance)
-        self.func()
-        
-        
-    def withdrawl(self):
-        user_pin = input("Enter Pin")
-        if user_pin == self.pin:
-            amount = int(input("how many Amount withdrawl"))
-            
-            if amount <= self.balance:
-                self.balance = self.balance - amount
-                print('withdrowl secessfully', self.balance)
-                self.func()
-                
-                
-                
+# class Atm():
+#     # print('hello world')
     
-                    
-            
-                
-            
+#     def __init__(self):
+#         # print("hello world")
+#         self.pin = ''
+#         # self.balance = 0
+#         self.menu()
         
         
         
+#         # method 
+#     def menu(self):
+#         # print("create pin")
+#         user_pin = input("""
+#                          1. create Pin
+#                          2. chanage pin
+#                          3. update pin 
+#                          4. check balance
+#                          """)
+        
+        
+#         if user_pin == '1':
+#             self.create_pin()
+        
+        
+        
+#         # method , if you function are inner side of class this is method 
+#         # and outside of class that is function 
+#     def create_pin(self):
+        
+#         user_pin = input("enter Pin")
+#         self.pin = user_pin
+#         print("create pin")
+    
+    
+    
+# Atm()
 
 
-Atm()
+
+L = [1, 2, 3]
+len(L)  # len is function 
+L.append(2)  # append is method, b/c L is class and append is method 
+print(L)
