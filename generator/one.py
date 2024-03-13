a6387015edb5b208f974c8c25ced33362f6d098a
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-13 11:32:50 +0530

    customise loop using generator

--- generator/one.py
+++ generator/one.py
@@ -17,14 +17,27 @@
 #     print(i)
 
 
-def generator():
-    for i in range(1, 10):
-        # print(i)
-        yield i
+# def generator():
+#     for i in range(1, 10):
+#         # print(i)
+#         yield i
 
 
-a = generator()
-print(next(a))
-print(next(a))
-for j in a:
-    print(j)
+# a = generator()
+# print(next(a))
+# print(next(a))
+# for j in a:
+#     print(j)
+
+
+# how to make loop using generator function 
+def generator(start, end):
+    for i in range(start, end):
+        yield i 
+
+
+for i in generator(1, 10):
+    print(i)
+
+
+    
