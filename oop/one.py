28988eafcf2f8e4a1a5c8f4f225be0aea2765816
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-13 19:03:58 +0530

    how to call metho str method automatically

--- oop/one.py
+++ oop/one.py
@@ -1,47 +1,16 @@
-#
-
-# class Atm():
-#     # print('hello world')
-    
-#     def __init__(self):
-#         # print("hello world")
-#         self.pin = ''
-#         # self.balance = 0
-#         self.menu()
-        
-        
-        
-#         # method 
-#     def menu(self):
-#         # print("create pin")
-#         user_pin = input("""
-#                          1. create Pin
-#                          2. chanage pin
-#                          3. update pin 
-#                          4. check balance
-#                          """)
-        
-        
-#         if user_pin == '1':
-#             self.create_pin()
-        
-        
-        
-#         # method , if you function are inner side of class this is method 
-#         # and outside of class that is function 
-#     def create_pin(self):
-        
-#         user_pin = input("enter Pin")
-#         self.pin = user_pin
-#         print("create pin")
-    
-    
-    
-# Atm()
 
 
-
-L = [1, 2, 3]
-len(L)  # len is function 
-L.append(2)  # append is method, b/c L is class and append is method 
-print(L)
+class Fraction:
+    
+    def __init__(self, x, y):
+        self.a = x 
+        self.b = y 
+        
+        
+    # str method is atomatically call 
+    def __str__(self):
+        return 'hello'
+    
+    
+a = Fraction(2, 2)
+print(a)
