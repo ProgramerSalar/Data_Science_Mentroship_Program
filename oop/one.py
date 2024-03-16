907ecb4293c6e7368691efbd4b5d060f421aceba
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-16 16:38:05 +0530

    how to make private varable|function AND how to access private function and variable

--- oop/one.py
+++ oop/one.py
@@ -1,15 +1,91 @@
 
 
-# Encapsulation = this means different different variable have different different data 
 
-# instance var -> python tutor
-class Person:
 
-  def __init__(self,name_input,country_input):
-    self.name = name_input
-    self.country = country_input
+class Atm:
 
-p1 = Person('nitish','india')
-p2 = Person('steve','australia')
-print(p1.name)
-print(p2.name)
+  # constructor(special function)->superpower -> 
+  def __init__(self):
+    print(id(self))
+    self.pin = ''
+    self.__balance = 0   # this is private method 
+    #self.menu()
+
+  
+    # this is private function 
+  def __menu(self):
+    user_input = input("""
+    Hi how can I help you?
+    1. Press 1 to create pin
+    2. Press 2 to change pin
+    3. Press 3 to check balance
+    4. Press 4 to withdraw
+    5. Anything else to exit
+    """)
+
+    if user_input == '1':
+      self.create_pin()
+    elif user_input == '2':
+      self.change_pin()
+    elif user_input == '3':
+      self.check_balance()
+    elif user_input == '4':
+      self.withdraw()
+    else:
+      exit()
+
+  def create_pin(self):
+    user_pin = input('enter your pin')
+    self.pin = user_pin
+
+    user_balance = int(input('enter balance'))
+    self.__balance = user_balance
+
+    print('pin created successfully')
+
+  def change_pin(self):
+    old_pin = input('enter old pin')
+
+    if old_pin == self.pin:
+      # let him change the pin
+      new_pin = input('enter new pin')
+      self.pin = new_pin
+      print('pin change successful')
+    else:
+      print('nai karne de sakta re baba')
+
+  def check_balance(self):
+    user_pin = input('enter your pin')
+    if user_pin == self.pin:
+      print('your balance is ',self.__balance)
+    else:
+      print('chal nikal yahan se')
+
+  def withdraw(self):
+    user_pin = input('enter the pin')
+    if user_pin == self.pin:
+      # allow to withdraw
+      amount = int(input('enter the amount'))
+      if amount <= self.__balance:
+        self.__balance = self.__balance - amount
+        print('withdrawl successful.balance is',self.__balance)
+      else:
+        print('abe garib')
+    else:
+      print('sale chor')
+      
+      
+      
+obj = Atm()
+obj.create_pin()
+obj._Atm__balance = "hehehe"
+obj.withdraw()
+
+# give this error 
+#TypeError: '<=' not supported between instances of 'int' and 'str'
+
+
+### you make private variable no Any one not axis the variable because your function and variable private but
+### IF YOU KNOW THE VARIABLE NAME, FUNCTION NAME THEN ACCESS THE YOUR VARIABLE AND FUNCTION OUT SIDE OF CLASS 
+### HOW CAN ACCESS THE VARIABLE AND FUNCTION IF ARE PRIVATE ?
+### EXAMPLE, YOU CLASS NAME IS ATM AND YOU PRIVATE FUNCTION IS VARIABLEIS BALANCE EX- _Atm_balance  
