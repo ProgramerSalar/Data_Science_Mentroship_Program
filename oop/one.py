beb459869d01d7215aa2e770a8d1e21467e81325
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-16 17:40:09 +0530

    static and instance variable

--- oop/one.py
+++ oop/one.py
@@ -1,33 +1,96 @@
-# list of objects
-class Person:
+class Atm:
 
-  def __init__(self,name,gender):
-    self.name = name
-    self.gender = gender
 
-p1 = Person('nitish','male')
-p2 = Person('ankit','male')
-p3 = Person('ankita','female')
+    # static variable - that variable in which found in class 
+  __counter = 1
 
-L = [p1,p2,p3]
+  # constructor(special function)->superpower -> 
+  def __init__(self):
+    print(id(self))
+    self.pin = ''
+    self.__balance = 0
+    # instance variable - that variable in which call of support of class that is instance varaible 
+    ## cid is static variable and counter is instantce variable 
+    self.cid = Atm.__counter
+    Atm.__counter = Atm.__counter + 1
+    #self.menu()
 
-for i in L:
-  print(i.name,i.gender)
-  
-  
-# dict of objects
-# list of objects
-class Person:
+  # utility functions
+  @staticmethod
+  def get_counter():
+    return Atm.__counter
 
-  def __init__(self,name,gender):
-    self.name = name
-    self.gender = gender
 
-p1 = Person('nitish','male')
-p2 = Person('ankit','male')
-p3 = Person('ankita','female')
+  def get_balance(self):
+    return self.__balance
 
-d = {'p1':p1,'p2':p2,'p3':p3}
+  def set_balance(self,new_value):
+    if type(new_value) == int:
+      self.__balance = new_value
+    else:
+      print('beta bahot maarenge')
 
-for i in d:
-  print(d[i].gender)
+  def __menu(self):
+    user_input = input("""
+    Hi how can I help you?
+    1. Press 1 to create pin
+    2. Press 2 to change pin
+    3. Press 3 to check balance
+    4. Press 4 to withdraw
+    5. Anything else to exit
+    """)
+
+    if user_input == '1':
+      self.create_pin()
+    elif user_input == '2':
+      self.change_pin()
+    elif user_input == '3':
+      self.check_balance()
+    elif user_input == '4':
+      self.withdraw()
+    else:
+      exit()
+
+  def create_pin(self):
+    user_pin = input('enter your pin')
+    self.pin = user_pin
+
+    user_balance = int(input('enter balance'))
+    self.__balance = user_balance
+
+    print('pin created successfully')
+
+  def change_pin(self):
+    old_pin = input('enter old pin')
+
+    if old_pin == self.pin:
+      # let him change the pin
+      new_pin = input('enter new pin')
+      self.pin = new_pin
+      print('pin change successful')
+    else:
+      print('nai karne de sakta re baba')
+
+  def check_balance(self):
+    user_pin = input('enter your pin')
+    if user_pin == self.pin:
+      print('your balance is ',self.__balance)
+    else:
+      print('chal nikal yahan se')
+
+  def withdraw(self):
+    user_pin = input('enter the pin')
+    if user_pin == self.pin:
+      # allow to withdraw
+      amount = int(input('enter the amount'))
+      if amount <= self.__balance:
+        self.__balance = self.__balance - amount
+        print('withdrawl successful.balance is',self.__balance)
+      else:
+        print('abe garib')
+    else:
+      print('sale chor')
+      
+      
+      
+    
