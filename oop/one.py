6f1685fdf7967d145e05c98cd98e8a647a8b5c27
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-16 17:11:56 +0530

    also work in dictionary

--- oop/one.py
+++ oop/one.py
@@ -13,3 +13,21 @@ L = [p1,p2,p3]
 
 for i in L:
   print(i.name,i.gender)
+  
+  
+# dict of objects
+# list of objects
+class Person:
+
+  def __init__(self,name,gender):
+    self.name = name
+    self.gender = gender
+
+p1 = Person('nitish','male')
+p2 = Person('ankit','male')
+p3 = Person('ankita','female')
+
+d = {'p1':p1,'p2':p2,'p3':p3}
+
+for i in d:
+  print(d[i].gender)
