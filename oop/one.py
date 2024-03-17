61894ddbdd9b9f87824ea8d824c1684ff0ecca79
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-17 15:54:29 +0530

    confusing input object

--- oop/one.py
+++ oop/one.py
@@ -1,19 +1,13 @@
-class Parent:
+class A:
+    def __init__(self):
+        self.var1=100
 
-    def __init__(self,num):
-        self.__num=num
+    def display1(self,var1):
+        print("class A :", self.var1)
+class B(A):
+  
+    def display2(self,var1):
+        print("class B :", self.var1)
 
-    def get_num(self):
-        return self.__num
-
-class Child(Parent):
-
-    def __init__(self,val,num):
-        self.__val=val
-
-    def get_val(self):
-        return self.__val
-        
-son=Child(100,10)
-print("Parent: Num:",son.get_num())
-print("Child: Val:",son.get_val())
+obj=B()
+obj.display1(200)
