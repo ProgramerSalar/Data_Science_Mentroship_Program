78db899f67338abbdb83952de88079ff026c3e74
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-17 16:29:30 +0530

    super constructor

--- oop/one.py
+++ oop/one.py
@@ -1,4 +1,4 @@
-# Method Overriding
+# super -> constuctor
 class Phone:
     def __init__(self, price, brand, camera):
         print ("Inside phone constructor")
@@ -6,17 +6,19 @@ class Phone:
         self.brand = brand
         self.camera = camera
 
-    def buy(self):
-        print ("Buying a phone")
-
 class SmartPhone(Phone):
-    def buy(self):
-        print ("Buying a smartphone")
+    def __init__(self, price, brand, camera, os, ram):
+        print('Inside smartphone constructor')
+        super().__init__(price, brand, camera)
+        self.os = os
+        self.ram = ram
+        print ("Inside smartphone constructor")
 
-s=SmartPhone(20000, "Apple", 13)
+s=SmartPhone(20000, "Samsung", 12, "Android", 2)
 
-s.buy()
+print(s.os)
+print(s.brand)
 
 
 
-## if you use same function in parent and child class then overhide the parent class constructor 
+# if you can use super then also call the contaructor 
