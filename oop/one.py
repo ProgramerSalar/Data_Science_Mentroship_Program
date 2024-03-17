7902cc72ad1282019c4babfeb9695bca3113919c
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-17 15:52:23 +0530

    get method in inheritance

--- oop/one.py
+++ oop/one.py
@@ -1,19 +1,16 @@
-# child can't access private members of the class
+class Parent:
 
-class Phone:
-    def __init__(self, price, brand, camera):
-        print ("Inside phone constructor")
-        self.__price = price
-        self.brand = brand
-        self.camera = camera
+    def __init__(self,num):
+        self.__num=num
 
-    #getter
+    def get_num(self):
+        return self.__num
+
+class Child(Parent):
+
     def show(self):
-        print (self.__price)
-
-class SmartPhone(Phone):
-    def check(self):
-        print(self.__price)
-
-s=SmartPhone(20000, "Apple", 13)
-s.show()
+        print("This is in child class")
+        
+son=Child(100)
+print(son.get_num())
+son.show()
