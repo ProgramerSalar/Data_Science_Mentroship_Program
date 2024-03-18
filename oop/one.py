39a34c79cbc946420109d6bc2882a9e90cc5f5ed
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-18 11:21:08 +0530

    how to call parent call function in child

--- oop/one.py
+++ oop/one.py
@@ -1,98 +1,23 @@
-# super -> constuctor
-class Phone:
-    def __init__(self, price, brand, camera):
-        print ("Inside phone constructor")
-        self.__price = price
-        self.brand = brand
-        self.camera = camera
-
-class SmartPhone(Phone):
-    def __init__(self, price, brand, camera, os, ram):
-        print('Inside smartphone constructor')
-        super().__init__(price, brand, camera)
-        self.os = os
-        self.ram = ram
-        print ("Inside smartphone constructor")
-
-s=SmartPhone(20000, "Samsung", 12, "Android", 2)
-
-print(s.os)
-print(s.brand)
-
 
 class Parent:
-
-    def __init__(self,num):
-      self.__num=num
-
-    def get_num(self):
-      return self.__num
-
-class Child(Parent):
   
-    def __init__(self,num,val):
-      super().__init__(num)
-      self.__val=val
-
-    def get_val(self):
-      return self.__val
-      
-son=Child(100,200)
-print(son.get_num())
-print(son.get_val())
-
-
-class Parent:
-    def __init__(self):
-        self.num=100
-
+  def __init__(self, num):
+    self.__num = num 
+    
+  def get_num(self):
+    return self.__num 
+  
 class Child(Parent):
-
-    def __init__(self):
-        super().__init__()
-        self.var=200
-        
-    def show(self):
-        print(self.num)
-        print(self.var)
-
-son=Child()
-son.show()
+  def __init__(self, num, val):
+    super().__init__(num)
+    self.__val = val 
+    
+  def get_val(self):
+    return self.__val
+    
+  
 
 
-class Parent:
-    def __init__(self):
-        self.__num=100
-
-    def show(self):
-        print("Parent:",self.__num)
-
-class Child(Parent):
-    def __init__(self):
-        super().__init__()
-        self.__var=10
-
-    def show(self):
-        print("Child:",self.__var)
-
-obj=Child()
-obj.show()
-
-
-class Parent:
-    def __init__(self):
-        self.__num=100
-
-    def show(self):
-        print("Parent:",self.__num)
-
-class Child(Parent):
-    def __init__(self):
-        super().__init__()
-        self.__var=10
-
-    def show(self):
-        print("Child:",self.__var)
-
-obj=Child()
-obj.show()
+a = Child(100, 299)
+print(a.get_num())
+print(a.get_val())
