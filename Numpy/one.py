8343f93b5c831b9a1cd9ef986b72681070e8ad77
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-18 22:05:54 +0530

    arange method in numpy

--- Numpy/one.py
+++ Numpy/one.py
@@ -1,20 +1,33 @@
 import numpy as np 
 
-a = np.array([1,2,3])
-print(a)
+# a = np.array([1,2,3])
+# print(a)
 
-# 2D array || matrix
-a1 = np.array([[1, 2, 3],[4, 5, 6]])
-print(a1)
-
-
-# 3D array || Tensor 
-a2 = np.array([[[1, 2],[3, 4]], [[5, 6], [7, 8]]])
-print(a2)
+# # 2D array || matrix
+# a1 = np.array([[1, 2, 3],[4, 5, 6]])
+# print(a1)
 
 
-# dType 
-b = np.array([1, 2, 3], dtype='float')
-print(b)
+# # 3D array || Tensor 
+# a2 = np.array([[[1, 2],[3, 4]], [[5, 6], [7, 8]]])
+# print(a2)
+
+
+# # dType 
+# b = np.array([1, 2, 3], dtype=float)
+# b1 = np.array([1, 2, 3], dtype=bool)
+# b2 = np.array([1, 2, 3], dtype=complex)
+
+# print(b)
+# print(b1)
+# print(b2)
+
+
+
+c = np.arange(10)  # range method in numpy is arange 
+c1 = np.arange(1, 10, 2)
+print(c1)
+print(c)
+
 
 
