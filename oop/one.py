9f82019bbfcf258fcebe67ede865ebd4ff495310
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-18 11:28:02 +0530

    how to set object in variable

--- oop/one.py
+++ oop/one.py
@@ -1,23 +1,30 @@
 
+
+
 class Parent:
   
-  def __init__(self, num):
-    self.__num = num 
+  def __init__(self):
+    self.num = 100
     
-  def get_num(self):
-    return self.__num 
-  
+    
 class Child(Parent):
-  def __init__(self, num, val):
-    super().__init__(num)
-    self.__val = val 
-    
-  def get_val(self):
-    return self.__val
-    
   
-
-
-a = Child(100, 299)
-print(a.get_num())
-print(a.get_val())
+  def __init__(self):
+    super().__init__()
+    self.val = 200
+    
+    
+  def show(self):
+    # return self.num, self.val
+    print(self.num)
+    print(self.val)
+    
+    
+    
+    
+    
+a = Parent()
+print(a.num)
+b = Child()
+print(b.val)
+print(b.show())
