525ab134eccb77e10b4a175439f48e0447f4f1c0
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-18 12:22:19 +0530

    single inheritance

--- oop/one.py
+++ oop/one.py
@@ -1,30 +1,17 @@
 
 
-
 class Parent:
   
-  def __init__(self):
-    self.num = 100
+  def __init__(self, name):
+    self.name = name 
     
     
 class Child(Parent):
-  
-  def __init__(self):
-    super().__init__()
-    self.val = 200
-    
-    
-  def show(self):
-    # return self.num, self.val
-    print(self.num)
-    print(self.val)
-    
-    
-    
-    
-    
-a = Parent()
-print(a.num)
-b = Child()
-print(b.val)
-print(b.show())
+  pass 
+
+
+
+
+
+a = Parent('ram')
+print(a.name)
