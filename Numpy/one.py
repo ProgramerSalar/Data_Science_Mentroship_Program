5847bb5f16f15a095e1c7dd24a4893786862f0bb
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-19 17:33:02 +0530

    numpy array Function max/min/sum/prod

--- Numpy/one.py
+++ Numpy/one.py
@@ -1,6 +1,6 @@
 
 
-## Array Attribute 
+## Array Function 
 import numpy as np 
 
 
@@ -9,23 +9,10 @@ a2 = np.arange(12, 24)
 # print(a1)
 # print(a2)
 
-## scalar operation 
-## arthamatic operation 
-
-# print(a1 * 2)   # 2 is scalar 
 
 
-
-### Relational operation 
-# print(a1 > 20)
-print(a1 == 10)  # in array which item are equal to 10 then give the true otherwise false    
-# print(a1 < 30)
-
-
-
-
-
-### vector operation 
-## arthmatic operation 
-## if you are calculate the data in two numpy array that is vector operation otherwise scalar operation 
-print(a1 + a2)
+# max/min/sum/prod 
+print(np.max(a1))
+print(np.min(a1))
+print(np.sum(a1))    # all item sum 
+print(np.prod(a1))   # prod means mroduct, all item calculate 
