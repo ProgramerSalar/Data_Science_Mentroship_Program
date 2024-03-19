c87742dd521093ef121a742e6a50466f21f12d00
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-19 17:56:42 +0530

    dot product

--- Numpy/one.py
+++ Numpy/one.py
@@ -4,22 +4,13 @@
 import numpy as np 
 
 
-a1 = np.random.random((3, 3))
+a1 = np.arange(12).reshape(3, 4)
+a2 = np.arange(12, 24).reshape(4, 3)
+
 # print(a1)
+# print(a2)
 
 
-# 0 -> col || 1 -> row 
-# print(np.max(a1, axis=0))   # colum wise maximum number 
 
-
-# mean/median/std/var   # std -> standard daviation # var -> variance 
-# print(np.mean(a1))
-# print(np.median(a1))
-# print(np.std(a1))
-# print(np.var(a1))
-
-
-## trigomatric function 
-print(np.sign(a1))
-print(np.cos(a1))
-print(np.tan(a1))
+## dot product 
+print(np.dot(a1, a2))
