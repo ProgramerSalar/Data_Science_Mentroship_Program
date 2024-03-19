1b01617c5da2f60c362616ff456112e6c1652928
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-19 16:38:12 +0530

    numpy size and itemsize attrubute

--- Numpy/one.py
+++ Numpy/one.py
@@ -10,20 +10,32 @@ a2 = np.arange(12).reshape(3, 4)
 a3 = np.arange(12).reshape(2, 3, 2)
 # print(a1)
 # print(a2)
-print(a3)
+# print(a3)
 
 # number of dimension
 # how many dimension of array  
-print(a1.ndim)
+# print(a1.ndim)
 
 
 
 
 # shape 
-# how many item preasent in a1 array 
-print(a1.shape)
-print(a2.shape)
-print(a3.shape)
+# how many item preasent in a1 array not calculate wise 
+# print(a1.shape)
+# print(a2.shape)
+# print(a3.shape)
 
 
 
+# size 
+# how many item present in array in calculate wise 
+print(a1.size)
+print(a2.size)
+print(a3.size)
+
+
+# itemsize 
+# how many mamory consume the array in biytes 
+print(a1.itemsize)
+print(a2.itemsize)
+print(a3.itemsize)
