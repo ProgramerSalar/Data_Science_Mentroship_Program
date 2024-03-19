56772a352b3c590399b400f6a3d86632ce3d1052
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-19 17:26:58 +0530

    relational operation and vector operation

--- Numpy/one.py
+++ Numpy/one.py
@@ -12,9 +12,20 @@ a2 = np.arange(12, 24)
 ## scalar operation 
 ## arthamatic operation 
 
-# print(a1 * 2)
-# print(a1 + a2)
+# print(a1 * 2)   # 2 is scalar 
+
 
 
 ### Relational operation 
-print(a1 > 20)
+# print(a1 > 20)
+print(a1 == 10)  # in array which item are equal to 10 then give the true otherwise false    
+# print(a1 < 30)
+
+
+
+
+
+### vector operation 
+## arthmatic operation 
+## if you are calculate the data in two numpy array that is vector operation otherwise scalar operation 
+print(a1 + a2)
