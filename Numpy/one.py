e4a432953bb2da9eb26b7a7732cbb7792ffd6004
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-19 17:21:44 +0530

    arthmatic operation and relational operation

--- Numpy/one.py
+++ Numpy/one.py
@@ -3,20 +3,18 @@
 ## Array Attribute 
 import numpy as np 
 
-a1 =np.arange(5)
-a2 = np.arange(12).reshape(3, 4)
-# The first dimension has 2 subarrays.
-# Each subarray has 3 rows and 2 columns.
-a3 = np.arange(12, dtype=float).reshape(2, 3, 2)
+
+a1 = np.arange(12)
+a2 = np.arange(12, 24)
 # print(a1)
 # print(a2)
-# print(a3)
+
+## scalar operation 
+## arthamatic operation 
+
+# print(a1 * 2)
+# print(a1 + a2)
 
 
-
-## changing datatype 
-## a3 data Type is float but not dataType is int32
-## what is the need of this things: if you change the dataType then you reduce the consume of mamery pawer 
-print(a3.astype(np.int32))
-
-
+### Relational operation 
+print(a1 > 20)
