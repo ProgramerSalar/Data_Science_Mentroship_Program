23d884d00921208c3f10b16c72029784657b7e58
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-19 18:04:21 +0530

    round/floor/ceil

--- Numpy/one.py
+++ Numpy/one.py
@@ -18,5 +18,17 @@ a2 = np.arange(12, 24).reshape(4, 3)
 
 
 ## log and exponent 
-print(np.log(a1))
-print(np.exp(a1))
+# print(np.log(a1))
+# print(np.exp(a1))
+
+
+
+## round/floor/ceil 
+b = 1.2
+b1 = 1.6 
+
+print(np.round(b))
+print(np.floor(b))
+print(np.floor(b1))  # output -> 1 
+print(np.ceil(b1))   # output -> 2 
+print(np.ceil(b))
