d5cf38d75be223e1d413c4860a5c48170ad89dd5
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-19 17:07:29 +0530

    dtype attrubute

--- Numpy/one.py
+++ Numpy/one.py
@@ -29,13 +29,22 @@ a3 = np.arange(12).reshape(2, 3, 2)
 
 # size 
 # how many item present in array in calculate wise 
-print(a1.size)
-print(a2.size)
-print(a3.size)
+# print(a1.size)
+# print(a2.size)
+# print(a3.size)
 
 
 # itemsize 
 # how many mamory consume the array in biytes 
-print(a1.itemsize)
-print(a2.itemsize)
-print(a3.itemsize)
+# print(a1.itemsize)
+# print(a2.itemsize)
+# print(a3.itemsize)
+
+
+
+
+## dtype 
+## what is the type of array ex:- float, int
+print(a1.dtype)
+print(a2.dtype)
+print(a3.dtype)
