d98eb8705db131242c4e1759b6f3793204fe7636
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-19 18:21:29 +0530

    indexing in numpy array

--- Numpy/one.py
+++ Numpy/one.py
@@ -6,29 +6,19 @@ import numpy as np
 
 a1 = np.arange(12).reshape(3, 4)
 a2 = np.arange(12, 24).reshape(4, 3)
+a3 = np.arange(8).reshape(2, 2, 2)
+# print(a3)
 
 # print(a1)
 # print(a2)
 
 
+## Indexing and Slicing 
+## indexing -> ek bar me ek hi item ko fetch karte hai 
+## Slicing -> ek bar me multiple item ko nikal sakte ho 
+print(a2)
+print(a2[1, 2])   # 1st row and 2 -> column 
 
-## dot product 
-# print(np.dot(a1, a2))
+print(a3)
+print(a3[1, 0, 1])   # 1 -> kon sa 2d array || 0 -> row || 1 -> col
 
-
-
-## log and exponent 
-# print(np.log(a1))
-# print(np.exp(a1))
-
-
-
-## round/floor/ceil 
-b = 1.2
-b1 = 1.6 
-
-print(np.round(b))
-print(np.floor(b))
-print(np.floor(b1))  # output -> 1 
-print(np.ceil(b1))   # output -> 2 
-print(np.ceil(b))
