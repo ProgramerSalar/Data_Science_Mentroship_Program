fa9371273e37ef82cfa7b991f0abc422bdf0424e
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-20 10:16:48 +0530

    boolean Indexing

--- Numpy/one.py
+++ Numpy/one.py
@@ -6,8 +6,8 @@ import numpy as np
 
 
 ## Advance Indexing 
-a = np.arange(24).reshape(6, 4)
-print(a)
+a = np.arange(240)
+# print(a)
 
 
 ### Fancy Indexing 
@@ -18,12 +18,25 @@ print(a)
 #  [12 13 14 15]
 #  [16 17 18 19]
 #  [20 21 22 23]]
-# i want to 1 row 2, 4, 5
-print(a[[1, 2, 4, 5]])
 
-# i want to 0, 1 and 3 column 
-print(a[:,[0, 1, 3]])  # : -> all row and column
 
 
 ### Boolean Indexing 
+
+# find all number is greter than 50 
+print(a[a > 50])
+
+
+# find the even number
+print(a[a % 2 == 0])
+
+
+# find the even number and greter than 50 
+b = a[(a > 50) & (a % 2 == 0)]
+print(b)
+
+
+# find the all number not divisible by 6
+b1 = a[~(a % 2 == 0)]   # ~ -> not divisible by 2 
+print(b1)
 
