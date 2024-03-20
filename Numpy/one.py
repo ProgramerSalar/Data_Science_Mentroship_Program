71d86bb27d0e0c7948e5f81ab56b84e338a00b67
ProgramerSalar <manishkumar60708090@gmail.com>
2024-03-20 15:08:01 +0530

    numpy concatenate

--- Numpy/one.py
+++ Numpy/one.py
@@ -4,19 +4,21 @@
 import numpy as np 
 
 
-###### append ########
-# np.append
-# The numpy.append() appends values along the mentioned axis at the end of the array
-# https://numpy.org/doc/stable/reference/generated/numpy.append.html
+###### concatenate ########
+# np.concatenate
+# numpy.concatenate() function concatenate a sequence of arrays along an existing axis.
+
+# https://numpy.org/doc/stable/reference/generated/numpy.concatenate.html
+
+
+a = np.arange(6).reshape(2, 3)
+b = np.arange(6, 12).reshape(2, 3)
+
+c = np.concatenate((a, b), axis=0)
+print(c)
+d = np.concatenate((a, b), axis=1)    
+print(d)
 
 
 
 
-a = np.arange(20)
-a2 = np.arange(24).reshape(6, 4)
-# print(a)
-print(a2)
-
-# print(np.append(a, 200))
-b = np.append(a2,np.random.random((a2.shape[0],1)),axis=1)   # axis=1 -> column wise 
-print(b)
